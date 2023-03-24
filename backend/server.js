a74057e2edc9bf6690fe239a7dc368b338a63336
Arttu Lahtinen <alahtin@epsa.upv.es>
2023-03-24 22:57:59 +0100

    added cors to server.js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware');
@@ -9,6 +10,9 @@ connectDB();
 
 const app = express();
 
+app.use(cors({
+    origin: '*', // allow to server to accept request from different origin
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
