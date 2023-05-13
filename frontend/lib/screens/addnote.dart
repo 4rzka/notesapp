c1df8a5ac7bbe0d9438cdfd6af8a0bde8a1e4510
Arttu Lahtinen <alahtin@epsa.upv.es>
2023-05-13 21:37:15 +0200

    ui update

--- frontend/lib/screens/addnote.dart
+++ frontend/lib/screens/addnote.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/models/note.dart';
+import 'package:frontend/models/todo.dart';
 import 'package:provider/provider.dart';
 import '../providers/notes_provider.dart';
 
@@ -19,6 +20,7 @@ class _AddNotePageState extends State<AddNotePage> {
 
   final FocusNode noteFocus = FocusNode();
   List<String> tags = [];
+  List<Todo> checkboxes = [];
 
   void addNewNote() {
     Note newNote = Note(
@@ -55,6 +57,15 @@ class _AddNotePageState extends State<AddNotePage> {
     });
   }
 
+  void addTodo() {
+    setState(() {
+      checkboxes.add(Todo(
+        name: '',
+        isChecked: false,
+      ));
+    });
+  }
+
   @override
   void initState() {
     if (widget.isUpdate) {
@@ -72,6 +83,7 @@ class _AddNotePageState extends State<AddNotePage> {
     return Scaffold(
         appBar: AppBar(
           actions: [
+            const IconButton(onPressed: null, icon: Icon(Icons.push_pin)),
             IconButton(
               onPressed: () {
                 // title and content should not be empty
@@ -87,96 +99,157 @@ class _AddNotePageState extends State<AddNotePage> {
               },
               icon: const Icon(Icons.save),
             ),
+            const IconButton(onPressed: null, icon: Icon(Icons.more_vert)),
           ],
         ),
         body: SafeArea(
-            child: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 20,
-            vertical: 10,
-          ),
-          child: Column(
-            children: [
-              TextField(
-                controller: titleController,
-                autofocus: (widget.isUpdate) ? false : true,
-                onSubmitted: (val) {
-                  if (val != '') {
-                    noteFocus.requestFocus();
-                  }
-                },
-                style: const TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 10,
+            ),
+            child: Column(
+              children: [
+                TextField(
+                  controller: titleController,
+                  autofocus: (widget.isUpdate) ? false : true,
+                  onSubmitted: (val) {
+                    if (val != '') {
+                      noteFocus.requestFocus();
+                    }
+                  },
+                  style: const TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  decoration: const InputDecoration(
+                    hintText: 'Title',
+                    border: InputBorder.none,
+                  ),
                 ),
-                decoration: const InputDecoration(
-                  hintText: 'Title',
-                  border: InputBorder.none,
-                ),
-              ),
-              Expanded(
-                  child: TextField(
-                focusNode: noteFocus,
-                controller: contentController,
-                maxLines: null,
-                style: const TextStyle(
-                  fontSize: 20,
-                ),
-                decoration: const InputDecoration(
-                  hintText: 'Note',
-                  border: InputBorder.none,
-                ),
-              )),
-              Row(
-                children: [
-                  Expanded(
-                      child: TextField(
-                    controller: tagController,
-                    onSubmitted: (val) {
-                      if (val != '') {
-                        setState(() {
-                          tags.add(val);
-                        });
-                        tagController.clear();
-                      }
-                    },
-                    decoration: const InputDecoration(
-                      hintText: 'Add Tag',
-                      border: InputBorder.none,
+                Expanded(
+                    child: TextField(
+                  focusNode: noteFocus,
+                  controller: contentController,
+                  maxLines: null,
+                  style: const TextStyle(
+                    fontSize: 20,
+                  ),
+                  decoration: const InputDecoration(
+                    hintText: 'Note',
+                    border: InputBorder.none,
+                  ),
+                )),
+                Row(
+                  children: [
+                    Expanded(
+                        child: TextField(
+                      controller: tagController,
+                      onSubmitted: (val) {
+                        if (val != '') {
+                          setState(() {
+                            tags.add(val);
+                          });
+                          tagController.clear();
+                        }
+                      },
+                      decoration: const InputDecoration(
+                        hintText: 'Add Tag',
+                        border: InputBorder.none,
+                      ),
+                    )),
+                    const SizedBox(
+                      width: 10,
                     ),
-                  )),
-                  const SizedBox(
-                    width: 10,
+                    ElevatedButton(
+                      onPressed: (() {
+                        if (tagController.text != '') {
+                          addTag();
+                        }
+                      }),
+                      child: const Text('Add'),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                if (tags.isNotEmpty)
+                  Wrap(
+                    children: tags
+                        .map((tag) => Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Chip(
+                                label: Text(tag),
+                                onDeleted: () {
+                                  setState(() {
+                                    tags.remove(tag);
+                                  });
+                                },
+                              ),
+                            ))
+                        .toList(),
                   ),
-                  ElevatedButton(
-                    onPressed: (() {
-                      if (tagController.text != '') {
-                        addTag();
-                      }
-                    }),
-                    child: const Text('Add'),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 20),
-              if (tags.isNotEmpty)
-                Wrap(
-                  children: tags
-                      .map((tag) => Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Chip(
-                              label: Text(tag),
-                              onDeleted: () {
+                if (checkboxes.isNotEmpty)
+                  Column(
+                    children: checkboxes.map((todo) {
+                      return Row(
+                        children: [
+                          Checkbox(
+                            value: todo.isChecked,
+                            onChanged: (value) {
+                              setState(() {
+                                todo.isChecked = value!;
+                              });
+                            },
+                          ),
+                          Expanded(
+                            child: TextField(
+                              onChanged: (value) {
                                 setState(() {
-                                  tags.remove(tag);
+                                  todo.name = value;
                                 });
                               },
+                              decoration: const InputDecoration(
+                                hintText: 'Todo',
+                                border: InputBorder.none,
+                              ),
                             ),
-                          ))
-                      .toList(),
-                )
+                          ),
+                          const SizedBox(width: 10),
+                        ],
+                      );
+                    }).toList(),
+                  ),
+              ],
+            ),
+          ),
+        ),
+        bottomSheet: Container(
+          height: 50,
+          width: double.infinity,
+          color: Colors.blueAccent,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton(
+                onPressed: () {
+                  addTodo();
+                },
+                icon: const Icon(Icons.check_box_outlined),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.mic),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.photo),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.attach_file),
+              ),
             ],
           ),
-        )));
+        ));
   }
 }
