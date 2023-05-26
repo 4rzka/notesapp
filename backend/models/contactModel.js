157a7b03835509358489e5e39efbc71e008a672c
Arttu Lahtinen <alahtin@epsa.upv.es>
2023-05-26 13:55:28 +0200

    edited contactmodel

--- backend/models/contactModel.js
+++ backend/models/contactModel.js
@@ -1,6 +1,28 @@
 const mongoose = require('mongoose');
 
 const contactSchema = mongoose.Schema({
+    // phone contact just as it is in the phone
+    firstname: {
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String,
+        required: false
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: false
+    },
+    // contact created by the user in the app
+    address: {
+        type: String,
+        required: false
+    },
     user: {
         type: mongoose.Schema.Types.ObjectId,
         required: true,
